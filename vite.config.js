fe89b3c51969b31c61994d702c051d117d9a8ce2
cxw668 <2070873373@qq.com>
2026-01-03 01:13:54 +0800

    chore: fix the reposity path

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/food-frontend/',
+  base: '/foodsys/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
