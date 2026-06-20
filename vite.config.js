0bccdc0aa639a5cbfa885057beb75897c0c6b796
cxw668 <2070873373@qq.com>
2026-06-21 02:34:44 +0800

    chore:vercel subpath

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: './',
+  base: '/foodsys/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
